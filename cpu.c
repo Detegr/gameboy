0f17b4543fef0cf4612cd1cac1fb0e1f28069f2a
Detegr <detegr@gmail.com>
2012-10-07 19:38:22 +0300

    Fix typo

--- cpu.c
+++ cpu.c
@@ -90,7 +90,7 @@ void ADDHLBC(CPU* c, MMU* m)
 {
 	c->reg.F &= ~SUBTRACT;
 	uint16_t hl = WORD(c->reg.H, c->reg.L);
-	uint16_t result = hl += WORD(c->reg.B, c->reg.C);
+	uint16_t result = hl + WORD(c->reg.B, c->reg.C);
 	if(result < hl) c->reg.F |= CARRY;
 	if(c->reg.F & CARRY || (result & 0x0FFF) < (hl & 0x0FFF))
 	{// Carry from bit 11
